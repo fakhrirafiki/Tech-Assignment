edd3b3f0ce5c32a11cd74ac758adf8266667ba21
Fakhri Rafiki <68749023+fakhrirafiki@users.noreply.github.com>
2021-06-10 14:29:05 +0700

    add x api key

--- src/config/Axios/index.js
+++ src/config/Axios/index.js
@@ -2,9 +2,13 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: `${process.env.REACT_APP_AUTH_URL}`,
-  headers: {
-    Authorization: `${process.env.REACT_APP_NEWS_API_KEY}`,
-  },
+  // headers: {
+  //   Authorization: `${process.env.REACT_APP_NEWS_API_KEY}`,
+  // },
 });
+
+instance.defaults.headers.common = {
+  "X-API-Key": `${process.env.REACT_APP_NEWS_API_KEY}`,
+};
 
 export default instance;
